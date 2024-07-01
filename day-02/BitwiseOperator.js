28abaa76fc1c8ba71a83b4e01f294f993a1c87c9
Minh Nhat <19521952@gm.uit.edu.vn>
2024-07-01 19:00:48 +0700

    Edit file lab2

---
+++ day-02/BitwiseOperator.js
@@ -0,0 +1,15 @@
+if (getFirstNum() !=0 & getSecondNum() !=0){
+    console.log('All nums are not zero');
+}
+
+function getFirstNum(){
+    console.log('Calling 1st method...');
+    return 0;
+}
+
+function getSecondNum(){
+    console.log("Calling 2nd method...");
+    return 2;
+}
+
+// && neu trai sai thi phai ko lam
